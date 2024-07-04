896224ee8aa7c85deac5fc3efa6409a8665b02d2
ankit <ankitxrajpoot@gmail.com>
2024-07-04 13:07:24 +0530

    Added error code enums

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,4 +1,6 @@
 const {FlightService} = require('../services/index');
+const {SuccessCodes} = require('../utils/error-codes');
+
 
 const flightService = new FlightService();
 
@@ -15,7 +17,7 @@ const create = async (req,res) =>{
             price: req.body.price
         }
         const flight = await flightService.createFlight(flightRequestData); 
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
             data: flight,
             success: true,
             message: 'Successfully created a flight',
@@ -37,7 +39,7 @@ const getAll = async (req,res) =>{
 
     try {
         const response = await flightService.getAllFlightData(req.query); 
-        return res.status(201).json({
+        return res.status(SuccessCodes.OK).json({
             data: response,
             success: true,
             message: 'Successfully fetched all flight',
